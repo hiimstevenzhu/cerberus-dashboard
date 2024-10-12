8baf4318dec1b47e032c9373e92ee49dc7df791d
hiimstevenzhu <123613244+hiimstevenzhu@users.noreply.github.com>
2024-10-13 00:41:48 +0100

    completed cerberus

--- backend/asr/whisper.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", default="medium", help="Model to use", choices=["tiny", "base", "small", "medium", "large"])
-    
-    
--- backend/keyword_search/parse_helper.py
+++
@@ -1,8 +0,0 @@
-# This file deals with parsing text and removing semblances of non-alphanumeric characters.
-
-def cleanText(text:str) -> str:
-    cleanedText = ""
-    for char in text:
-        if char.isalnum() or char.isspace():
-            cleanedText += char
-    return " " + cleanedText + ""
